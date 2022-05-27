d9f425de96f50f2bee4523242a2e6c62d3ef1368
Metanonia <metanonia@outlook.kr>
2022-05-27 14:53:38 +0900

    ..

--- src/main/java/com/metanonia/jwtsample/controller/PrivateController.java
+++ src/main/java/com/metanonia/jwtsample/controller/PrivateController.java
@@ -1,6 +1,8 @@
 package com.metanonia.jwtsample.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/private")
 public class PrivateController {
     @GetMapping("/")
-    public String privateHome() {
+    public String privateHome(@AuthenticationPrincipal User pricipal) {
+        log.info(pricipal.toString());
         return "/private/home";
     }
 }
